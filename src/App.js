74b143eef6ce2f406bd45e11e8e97f37cef8dfbd
Daniel Cavalcante <danielcdesouza@gmail.com>
2021-03-02 21:52:52 -0300

    Add two new pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { CssBaseline } from "@material-ui/core";
+
+import { Login } from "./pages/login";
+import { Home } from "./pages/home";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <CssBaseline />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
